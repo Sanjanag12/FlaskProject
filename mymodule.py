646084e798e2b721f44c6a5ec6f6630f690c8c6b
Sanjana Shetty <134592355+Sanjanag12@users.noreply.github.com>
2023-08-02 12:27:20 +0530

    modified

--- mymodule.py
+++ mymodule.py
@@ -1,2 +1,9 @@
-def my_func():
-    print(" hey i am in mymodule.py")
+def func():
+    print('hello')
+
+print("top level")
+
+if __name__ =="__main__" :
+    print("one.py is being run directly")
+else:
+    print(" one.py has been imported")         
--- myprogram.py
+++ myprogram.py
@@ -1,4 +1,11 @@
-from mymodule import my_func
+import mymodule
 
 
-my_func()
+print("top level in two.py ")
+
+mymodule.func()
+
+if __name__ =="__main__" :
+    print("two.py is being run directly")
+else:
+    print(" two.py has been imported")
